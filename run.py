d6896f07fe7d8d0732d97fde2ba4e15ceaf5c835
ConwayC1987 <conwayciaran@gmail.com>
2023-04-06 14:26:55 +0000

    Add function to clear the screen

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import random
+
+
+# Function to clear the screen 
+def clear_terminal():
+    # For windows
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
